1628ed5770e4fedc5fd7184690adc9a076a1b048
yusuffomotoyosi <yusuffomotoyosi87@gmail.com>
2023-07-18 04:57:42 -0700

    6611

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,23 @@
 #include "main.h"
-
 /**
- * print_alphabet - Entry point
- * Return:0
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return: 0
  */
-
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-  char c = 'a';
+	int i = 0;
+	char c;
 
-  while (c <= 'z')
-    {
-      _putchar(c);
-      c++;
-
-    }
-  _putchar('\n');
-
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
+
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,22 @@
 #include "main.h"
 /**
-  * print_alphabet_x10 - repeats the print_alphabet 10 times.
-  * Return: Nothing.
-  */
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return: 0
+ */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i = 0;
 	char c;
 
-	for (i = 0; i < 10; ++i)
+	while (i < 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		c = 'a';
+		while (c <= 'z')
 		{
-		  _putchar(c);
+			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
