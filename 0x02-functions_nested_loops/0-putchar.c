c723e41a60916c2ce0c2c9b94a7efa906e8e79ff
yusuffomotoyosi <yusuffomotoyosi87@gmail.com>
2023-07-18 04:33:42 -0700

    fah

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,13 @@
  */
 int main(void)
 {
-        char c[] = "_putchar";
-	int i;
+      char c[] = "_putchar";
+      int i;
 
 	for (i = 0; i < 9; i++)
 	{
-	  _putchar(c[i]);
-
-  }
+		_putchar(c[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
