559947a8c99414976404fc388c3632fba704d400
yusuffomotoyosi <yusuffomotoyosi87@gmail.com>
2023-07-18 04:43:54 -0700

    2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * main - prints the string "Holberton" from a character array.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-      char c[] = "_putchar";
-      int i;
+	char c[] = "_putchar";
+	int i;
 
 	for (i = 0; i < 9; i++)
 	{
