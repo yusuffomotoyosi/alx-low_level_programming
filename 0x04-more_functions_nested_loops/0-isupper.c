9979495c5b2bce01f445195c9389b15856c0fe39
yusuffomotoyosi <yusuffomotoyosi87@gmail.com>
2023-07-20 04:39:04 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,10 @@
  *@c: tested character
  *Return: returns 1 if it is uppercase , 0 if not
  */
-
 int _isupper(int c)
 {
-  if ((c >= 'A') && (c <= 'Z'))
-  {
-    return (1);
-  }
-  
-  return (0);
+	if (c > 64 && c < 91)
+		return (1);
+
+	return (0);
 }
