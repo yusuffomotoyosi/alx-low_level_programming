556c72af86e14edb4d609c835bff74cb29c28cad
yusuffomotoyosi <yusuffomotoyosi87@gmail.com>
2023-07-20 04:49:34 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,14 +7,13 @@
  */
 void print_numbers(void)
 {
-  int i;
+	int i;
 
-  i = 0;
-  while (i < 10)
-    {
-      _putchar(i + '0');
-      i++;
-    }
-  _putchar('\n');
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
 }
-
