42d6b5438e4f7a49e28140dcfcdb6874a546d485
yusuffomotoyosi <yusuffomotoyosi87@gmail.com>
2023-07-20 06:04:07 -0700

    12

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_most_numbers - Print numbers 0 to 9, excluding 2 and 4.
- * Description: Can only use _putchar twice.
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
  */
 void more_numbers(void)
 {
